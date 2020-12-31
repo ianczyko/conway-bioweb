158ae1191906ca0cca8ce5129b50c897a847c452
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2020-12-31 17:04:57 +0000

    Untracking build directory

--- build_web/current/models.py
+++
@@ -1,7 +0,0 @@
-## @file current/models.py
-#  @brief current server status model
-
-"""current state module"""
-
-
-
--- build_web/version/version_gen.py
+++
@@ -1,7 +0,0 @@
-web_srv_prefix = "srvmyapp"
-major = 0
-minor = "09"
-compilation = 0
-DB_NAME = "mydb"
-DB_USER = "mydb"
-DB_PASSWORD = "mydb"

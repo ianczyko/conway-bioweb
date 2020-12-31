fb76aea190f0b20f6ef42c3a37fa020041ebd6c1
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2020-12-31 17:07:58 +0000

    Initial client/ config

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -11,62 +11,78 @@ angular.module('myAppControllers', [])
 						 $translate.use(lang);
 					 };
 				 }])
-	.controller('versionController', //versions of system modules
-				['$scope',
-				 'srvInfo',
-				 function($scope, srvInfo) {
-					 srvInfo.getVersion(
-					 	 function(data) {
-					 		 $scope.server_ver = data;
-					 	 });
-					 srvInfo.getCppNumber(
-						 function(data) {
-							 $scope.cpp_get = data;
-						 });
-					 $scope.client_ver = client_ver_major.toString() + '.' + client_ver_minor.toString() + '.' + client_ver_compilation.toString(); //from version.js file
-				 }])
-	.controller('commandController', //cpp commands
-				['$scope',
-				 'srvCommands',
-				 function($scope, srvCommands) {
-					 $scope.getCppCommandsNo = function() {
-						 srvCommands.getCppCommands(
-							 function(data) {
-								 $scope.cpp_commands_no = Object.keys(data).length;
-							 });
-					 };
-					 $scope.clickNewCommand = function() {
-						 srvCommands.startCommand(
-							 function(data) {
-								 $scope.getCppCommandsNo();
-							 })
-					 };
-					 $scope.getCppCommandsNo();
-				 }])
-	.controller('currentController', //current server params
-				['$scope',
-				 '$timeout',
-				 'srvInfo',
-				 'srvCommands',
-				 function($scope, $timeout, srvInfo, srvCommands) {
-					 var REFRESH_INTERVAL = 1000; //ms
+	// .controller('versionController', //versions of system modules
+	// 			['$scope',
+	// 			 'srvInfo',
+	// 			 function($scope, srvInfo) {
+	// 				 srvInfo.getVersion(
+	// 				 	 function(data) {
+	// 				 		 $scope.server_ver = data;
+	// 				 	 });
+	// 				 $scope.client_ver = client_ver_major.toString() + '.' + client_ver_minor.toString() + '.' + client_ver_compilation.toString(); //from version.js file
+	// 			 }])
+	// .controller('commandController', //cpp commands
+	// 			['$scope',
+	// 			 'srvCommands',
+	// 			 function($scope, srvCommands) {
+	// 				 $scope.getCppCommandsNo = function() {
+	// 					 srvCommands.getCppCommands(
+	// 						 function(data) {
+	// 							 $scope.cpp_commands_no = Object.keys(data).length;
+	// 						 });
+	// 				 };
+	// 				 $scope.clickNewCommand = function() {
+	// 					 srvCommands.startCommand(
+	// 						 function(data) {
+	// 							 $scope.getCppCommandsNo();
+	// 						 })
+	// 				 };
+	// 				 $scope.getCppCommandsNo();
+	// 			 }])
+	// .controller('currentController', //current server params
+	// 			['$scope',
+	// 			 '$timeout',
+	// 			 'srvInfo',
+	// 			 'srvCommands',
+	// 			 function($scope, $timeout, srvInfo, srvCommands) {
+	// 				 var REFRESH_INTERVAL = 1000; //ms
 
-					 var call = function() { //function called periodically
-						 $scope.getCppCommands = function() {
-							 srvCommands.getCppCommands(
-								 function(data) {
-									 $scope.cpp_commands = data;
-								 });
-						 };
+	// 				 var call = function() { //function called periodically
+	// 					 $scope.getCppCommands = function() {
+	// 						 srvCommands.getCppCommands(
+	// 							 function(data) {
+	// 								 $scope.cpp_commands = data;
+	// 							 });
+	// 					 };
 
-						 srvInfo.getCurrent(
-					 		 function(data) {
-					 			 $scope.current = data;
-								 $scope.getCppCommands();
-								 $timeout(call, REFRESH_INTERVAL);
-					 		 });
-					 };
-					 $timeout(call, 0); //start calling the service
-				 }]);
-
-
+	// 					 srvInfo.getCurrent(
+	// 				 		 function(data) {
+	// 				 			 $scope.current = data;
+	// 							 $scope.getCppCommands();
+	// 							 $timeout(call, REFRESH_INTERVAL);
+	// 				 		 });
+	// 				 };
+	// 				 $timeout(call, 0); //start calling the service
+	// 			 }])
+	.controller('conway',
+				'srvInfo',
+				function($scope, srvInfo) {
+					$scope.resetGrid = function() {
+						$scope.grid = Array($scope.rows).fill().map(() => Array($scope.columns).fill(false));
+					};
+					$scope.negateElement = function(column, row) {
+						$scope.grid[row][column] = !$scope.grid[row][column];
+					};
+					$scope.evolvebtn = function(){
+						srvInfo.evolve($scope.grid);
+					}
+					srvInfo.evolve(
+						grid,
+						function(data) {
+							$scope.grid = data;
+					});
+					$scope.rows = 32;
+					$scope.columns = 32;
+					$scope.resetGrid();
+				});
+				
--- client/js/services.js
+++ client/js/services.js
@@ -6,24 +6,27 @@ angular.module('myAppServices', [])
              function($http) {
                  this.baseURL = client_server_prefix + '/ajax/'; //the prefix defined in version.js
 
-                 this.getVersion = function(callback) {
-                     return $http.get(this.baseURL + 'version/get').success(callback);
-				 };
-                 this.getCurrent = function(callback) {
-                     return $http.get(this.baseURL + 'current/get').success(callback);
+                //  this.getVersion = function(callback) {
+                //      return $http.get(this.baseURL + 'version/get').success(callback);
+				//  };
+                //  this.getCurrent = function(callback) {
+                //      return $http.get(this.baseURL + 'current/get').success(callback);
+                //  };
+                 this.evolve = function(grid, callback) {
+                    data = {
+                        'grid' : grid
+                    }
+                    return $http.post(this.baseURL + 'conwaypy/evolve_request', data).success(callback);
                  };
-                 this.getCppNumber = function(callback) {
-                     return $http.get(this.baseURL + 'calcpy/getNumber').success(callback);
-                 };
-			 })
-	.service('srvCommands', //commands
-			 function($http) {
-                 this.baseURL = client_server_prefix + '/ajax/calcpy/'; //the prefix is defined in version.js
+			 });
+	// .service('srvCommands', //commands
+	// 		 function($http) {
+    //              this.baseURL = client_server_prefix + '/ajax/calcpy/'; //the prefix is defined in version.js
 
-                 this.getCppCommands = function(callback) {
-                     return $http.get(this.baseURL + 'getCommands').success(callback);
-                 };
-				 this.startCommand = function(callback) {
-					 return $http.get(this.baseURL + 'startCommand').success(callback);
-				 };
-             });
+    //              this.getCppCommands = function(callback) {
+    //                  return $http.get(this.baseURL + 'getCommands').success(callback);
+    //              };
+	// 			 this.startCommand = function(callback) {
+	// 				 return $http.get(this.baseURL + 'startCommand').success(callback);
+	// 			 };
+    //          });

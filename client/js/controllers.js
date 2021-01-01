fb45129234001f54a469f2899deb5da08b67bdbf
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2021-01-01 01:23:43 +0000

    Added simulation support and default grid

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -14,23 +14,58 @@ angular.module('myAppControllers', [])
 	.controller('conway',
 				['$scope',
 				'srvInfo',
-				function($scope, srvInfo) {
+				'$interval',
+				function($scope, srvInfo, $interval) {
 					$scope.resetGrid = function() {
+						$scope.generation = 0;
 						$scope.grid = Array($scope.rows).fill().map(() => Array($scope.columns).fill(false));
+					};
+					$scope.initialArrangement = function() {
+						$scope.resetGrid();
+						var mid_row = Math.floor($scope.rows / 2);
+						var mid_col = Math.floor($scope.columns / 2);
+						$scope.grid[mid_row-1][mid_col] = true;
+						$scope.grid[mid_row][mid_col] = true;
+						$scope.grid[mid_row+1][mid_col] = true;
+						$scope.grid[mid_row][mid_col-1] = true;
+						$scope.grid[mid_row-1][mid_col] = true;
+						$scope.grid[mid_row-1][mid_col+1] = true;
 					};
 					$scope.negateElement = function(column, row) {
 						$scope.grid[row][column] = !$scope.grid[row][column];
 					};
-					$scope.evolvebtn = function(){
+					$scope.evolve = function(){
+						$scope.generation++;
 						srvInfo.evolve(
 							$scope.grid,
 							function(data) {
 								$scope.grid = data.data.grid;
 						});
 					}
+					$scope.generation = 0;
+					var stop;
+					$scope.simulationIsOn = false;
+					$scope.startSimulation = function(){
+						if ( angular.isDefined(stop) ) return;
+						$scope.simulationIsOn = true;
+						stop = $interval(function() {
+							$scope.evolve()
+						}, 200);
+					}
+					$scope.stopSimulation = function(){
+						if (angular.isDefined(stop)) {
+							$scope.simulationIsOn = false;
+							$interval.cancel(stop);
+							stop = undefined;
+						}
+					}
+					$scope.$on('$destroy', function() {
+						// Make sure that the interval is destroyed too
+						$scope.stopSimulation();
+					});
 					$scope.rows = 32;
 					$scope.columns = 32;
-					$scope.resetGrid();
+					$scope.initialArrangement();
 				}])
 	
 				

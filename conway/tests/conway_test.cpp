35838042391d5ee762257b32597fdfee20b3052d
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2021-01-02 21:13:12 +0000

    Added tests

--- conway/tests/conway_test.cpp
+++ conway/tests/conway_test.cpp
@@ -8,17 +8,61 @@
 
 #include <boost/test/unit_test.hpp>
 
-
 #include "../src/conway.hpp"
 
 using namespace boost;
 using boost::unit_test::test_suite;
 
+BOOST_TEST_DONT_PRINT_LOG_VALUE(Grid)
+
 BOOST_AUTO_TEST_SUITE( conway_test )
 
-// BOOST_AUTO_TEST_CASE( Testttest )
-// {
-// 	BOOST_CHECK_EQUAL( ttest(), 12345 );
-// }
+BOOST_AUTO_TEST_CASE( AllDead )
+{
+   int rows = 3;
+   int cols = 3;
+   Grid initial(rows, std::vector<bool>(cols, false));
+	BOOST_CHECK_EQUAL( evolve(initial, 1), initial );
+}
+BOOST_AUTO_TEST_CASE( LonelinessDeath )
+{
+   int rows = 3;
+   int cols = 3;
+   Grid initial(rows, std::vector<bool>(cols, false));
+   Grid expected(rows, std::vector<bool>(cols, false));
+	initial[1][1] = true;
+   auto evolved = evolve(initial, 1);
+   BOOST_CHECK_EQUAL( evolved, expected );
+}
+BOOST_AUTO_TEST_CASE( OverpopulationDeath )
+{
+   int rows = 3;
+   int cols = 3;
+   Grid initial(rows, std::vector<bool>(cols, true));
+	initial[0][0] = false;
+	initial[0][2] = false;
+	initial[2][2] = false;
+	initial[2][0] = false;
+   auto evolved = evolve(initial, 1);
+   Grid expected(rows, std::vector<bool>(cols, true));
+   expected[1][1] = false;
+   BOOST_CHECK_EQUAL( evolved, expected );
+}
+BOOST_AUTO_TEST_CASE( Birth )
+{
+   // using a Blinker (Period 2 oscillator)
+   int rows = 3;
+   int cols = 3;
+   Grid initial(rows, std::vector<bool>(cols, false));
+	initial[0][1] = true;
+	initial[1][1] = true;
+	initial[2][1] = true;
+   auto evolved = evolve(initial, 1);
+   Grid expected(rows, std::vector<bool>(cols, false));
+   expected[1][0] = true;
+   expected[1][1] = true;
+   expected[1][2] = true;
+   BOOST_CHECK_EQUAL( evolved, expected );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
--- web/conwaypy/tests.py
+++ web/conwaypy/tests.py
@@ -8,41 +8,41 @@ from . import conway
 from . import views
 
 
-# class CalcPyLibraryTestCase(django.test.TestCase):
-#     """integration test, call C++ library interface from Python"""
+class ConwayPyLibraryTestCase(django.test.TestCase):
+    """integration test, call C++ library interface from Python"""
 
-#     def test01getNumber(self):
-#         """test the return number"""
-#         self.assertEqual( calc.getNumber(), 1234 )
-
-#     def test02command(self):
-#         """start/stop tick command test"""
-#         cmdmgr = calc.CommandManager()
-#         cmd_id = cmdmgr.start()
-#         self.assertNotEqual( cmdmgr.getState(cmd_id), calc.DONE )
-#         for i in range(100):
-#             time.sleep(0.1)
-#             if cmdmgr.getState(cmd_id) == calc.DONE:
-#                 break
-#         self.assertEqual( cmdmgr.getState(cmd_id), calc.DONE )
-#         self.assertEqual( len(cmdmgr.getIds()), 1 )
+    def test01evolve(self):
+        """test the return number"""
+        blinker_vertical = [
+            [False, True, False],
+            [False, True, False],
+            [False, True, False],
+        ]
+        blinker_horizontal = [
+            [False, False, False],
+            [True,  True,  True],
+            [False, False, False],
+        ]
+        self.assertEqual( conway.evolve(blinker_vertical), blinker_horizontal )
 
 
-# class CalcPyViewTestCase(django.test.TestCase):
-#     """module view test"""
+class ConwayPyViewTestCase(django.test.TestCase):
+    """module view test"""
 
-#     def test01getNumber(self):
-#         """check if service return proper dict"""
-#         self.assertEqual( views.getNumber({}), {'number': 1234})
+    def test01getNumber(self):
+        """check if service return proper dict"""
+        blinker_vertical = [
+            [False, True, False],
+            [False, True, False],
+            [False, True, False],
+        ]
+        blinker_horizontal = [
+            [False, False, False],
+            [True,  True,  True],
+            [False, False, False],
+        ]
+        self.assertEqual( views.evolve_request(blinker_vertical), blinker_horizontal)
 
-#     def test02getCommands(self):
-#         """check if service return proper dict"""
-#         self.assertEqual( views.getCommands({}),
-#                           {1: {'progress': 0.995, 'state': 'DONE'}})
-#         dict1 = views.getCommands({})
-#         views.startCommand({});
-#         dict2 = views.getCommands({})
-#         self.assertEqual( len(dict1) + 1, len(dict2) )
 
 
 

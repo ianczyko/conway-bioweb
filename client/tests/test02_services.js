cdfbd72eab452812b6f578356bcb553a30296834
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2021-01-03 17:46:16 +0000

    Configured js tests

--- client/tests/test02_services.js
+++ client/tests/test02_services.js
@@ -19,43 +19,10 @@ function functionResponseCheck(r) {
 	ok( typeof(r) != undefined && r.ala == "ala", "srv.py non-empty response testing" );
 }
 
-asyncTest( "service getVersion", function() {
-    expect( 1 );
-	var srv = injector.get('srvInfo');
-	srv.getVersion(functionResponseCheck);
-    setTimeout(
-		function() {
-			start();
-		},
-		100);
-});
-
-asyncTest( "service getCurrent", function() {
-    expect( 1 );
-	var srv = injector.get('srvInfo');
-	srv.getCurrent(functionResponseCheck);
-    setTimeout(
-		function() {
-			start();
-		},
-		100);
-});
-
 asyncTest( "service getCppNumber", function() {
     expect( 1 );
 	var srv = injector.get('srvInfo');
-	srv.getCppNumber(functionResponseCheck);
-    setTimeout(
-		function() {
-			start();
-		},
-		100);
-});
-
-asyncTest( "service getCppCommands", function() {
-    expect( 1 );
-	var srv2 = injector.get('srvCommands');
-	srv2.getCppCommands(functionResponseCheck);
+	srv.evolve([[true]], 1, functionResponseCheck);
     setTimeout(
 		function() {
 			start();
